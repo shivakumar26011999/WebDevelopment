0493bed495f82bbebb6c797266b9f58ce367e966
shiva <gandheshiva9@gmail.com>
2023-06-02 18:53:20 +0530

    JavaScript added.

--- calculator/js/index.js
+++ calculator/js/index.js
@@ -2,4 +2,25 @@
 
 console.log("Calculator...!");
 
-document.querySelector(".button7").addEventListener("click", function () {});
+const buttonContainer = document.querySelector(".proper");
+const userInput = document.querySelector(".textbox");
+const equalTo = document.querySelector(".equalto");
+const clearBtn = document.querySelector(".clear");
+
+buttonContainer.addEventListener("click", function (e) {
+  if (e.target.classList.contains("realbutton")) {
+    let currentValue = e.target.dataset.value;
+    userInput.value = userInput.value + currentValue;
+    // console.log(e.target.dataset.value);
+  }
+});
+
+clearBtn.addEventListener("click", function () {
+  userInput.value = "";
+});
+
+equalTo.addEventListener("click", function () {
+  const result = eval(userInput.value);
+  console.log(result);
+  userInput.value = result;
+}) 
